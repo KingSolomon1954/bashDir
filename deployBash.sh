40a214c6e61e98fbb022f71fd7f7c02118028e01
KingSolomon1954 <KingSolomon1954@gmail.com>
2018-11-11 13:41:00 -0800

    Preserve bash_profile during deploy if found

--- deployBash.sh
+++ deployBash.sh
@@ -13,4 +13,13 @@ fi
 
 # ln -s              TARGET       LINK_NAME
 symlinkFile $(pwd)/bashrc       $HOME/.bashrc
+
+# Preserve original .bash_profile for reference
+if [ ! -L "$HOME/.bash_profile" ]; then
+    if [ -e "$HOME/.bash_profile" ]; then
+        mv "$HOME/.bash_profile" "$HOME/.bash_profile.org"
+    fi
+fi
+
+# ln -s              TARGET       LINK_NAME
 symlinkFile $(pwd)/bash_profile $HOME/.bash_profile
