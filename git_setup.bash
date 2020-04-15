c2a835bdec9486d52bec21355ffd4f0bd84de35e
King Solomon <KingSolomon1954@gmail.com>
2020-04-14 20:59:30 -0700

    work-in-progress

--- git_setup.bash
+++ git_setup.bash
@@ -8,5 +8,10 @@ sourceIt /usr/local/etc/bash_completion.d/git-completion.bash
 
 alias glog="git log --all --oneline --decorate --graph"
 
+alias gs="git status"
+alias gc="git commit"
+alias gcw="git commit -m \"work-in-progress\""
+
+
 # -----------------------------------------------------------
 
